59251853a82d009112cdce3804c62f4141a94518
wmsJackWang <1042164771@qq.com>
2020-01-17 18:20:20 +0800

    年前提交

---
+++ jackdking-redistemplate/src/main/java/org/jackdking/redistemplate/JackdkingRedisTemplate.java
@@ -0,0 +1,17 @@
+package org.jackdking.redistemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class JackdkingRedisTemplate 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(JackdkingRedisTemplate.class, args);
+    }
+}
